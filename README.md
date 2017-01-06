eb689efa5cdb4b6c5815bd3ff755ccf4bbce1652
luckydrq <drqzju@gmail.com>
2017-01-06 15:19:26 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # html-self-closing-tags
 HTML self-closing tag list
+
+http://xahlee.info/js/html5_non-closing_tag.html
+
+## install
+```
+$ npm i html-self-closing-tags
+```
+
+## Lisence
+MIT
---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+
+// @see http://xahlee.info/js/html5_non-closing_tag.html
+module.exports = [
+  'area',
+  'base',
+  'br',
+  'col',
+  'command',
+  'embed',
+  'hr',
+  'img',
+  'input',
+  'keygen',
+  'link',
+  'meta',
+  'param',
+  'source',
+  'track',
+  'wbr',
+];
